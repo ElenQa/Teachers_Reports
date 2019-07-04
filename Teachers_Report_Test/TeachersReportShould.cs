9d9309e69b09f1abe361cdfb2d5aa4b28d988342
ElenQa <47454865+ElenQa@users.noreply.github.com>
2019-07-04 13:16:55 +0300

    Added  test Nunit

--- Teachers_Report_Test/TeachersReportShould.cs
+++ Teachers_Report_Test/TeachersReportShould.cs
@@ -12,26 +12,96 @@ namespace Teachers_Report_Test
     [TestFixture]
     public class TeachersReportShould
     {
+        string input_file_name1;
+        string input_file_name2;
+        string output_file_name;
+        string input_test_file_name1;
+        string input_test_file_name2;
+        string output_test_file_name;
+                
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            try
+            {
+                input_file_name1 = TestContext.Parameters["input_file_name1"].ToString();
+                input_file_name2 = TestContext.Parameters["input_file_name2"].ToString();
+                input_test_file_name1 = TestContext.Parameters["input_test_file_name1"].ToString();
+                input_test_file_name2 = TestContext.Parameters["input_test_file_name2"].ToString();
+                output_test_file_name = TestContext.Parameters["output_test_file_name"].ToString();
+
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Test can not be executed, because of missing input params" + e.Message);
+            }
+
+            output_file_name = TestContext.Parameters["output_file_name"].ToString();
+        }
 
         [Test]
         public void FinalFileExsist()
         {
-            string file_name = @"C:\Users\OChernovolyk\source\repos\Teachers_Reports\Teachers_Reports\bin\Debug\final.txt";
-
-            Assert.That(File.Exists(file_name));
+            
         }
 
         [Test]
-        public void AvarageResultCorrect()
+        [TestCase(72)]
+        public void AvarageResultCorrect(int ExpectedAvarageMark)
         {
+            try
+            {
+                Assert.That(File.Exists(output_file_name));
+            }
 
+            catch (Exception e)
+            {
+                Assert.Fail("Test can not be executed, because of missing input params" + e.Message);
+            }
+
+            Main_c student1 = new Main_c();
+            
+            student1.ReadMarks(input_test_file_name1);
+            student1.ReadMarks(input_test_file_name2);
+            
+            student1.CountAvarageMark(output_test_file_name);
+           
+            List<string> Mark = File.ReadAllLines(output_test_file_name).ToList();
+            int delim_pos = Mark[1].IndexOf('-');
+            int ActualMark = Convert.ToInt32(Mark[1].Substring(delim_pos + 1));
+            
+
+            Assert.That(ActualMark, Is.EqualTo(ExpectedAvarageMark));
         }
 
         [Test]
         public void NotReturnDuplicationStudents()
         {
+            List<string> Students = File.ReadAllLines(output_file_name).ToList();
+            List<string> Students_Names = new List<string>();
+            for (int i = 1; i < Students.Count; i++)
+            {
+              
+                int delim_pos = Students[i].IndexOf('-');
+                string name = Students[i].Substring(0, delim_pos);
+
+                foreach (string St in Students)
+                {
+                    Students_Names.Add(name);
+
+                    if (Students_Names.Contains(name)){
+                        break;
+                    }
+                        
+                }
+            }
+            
+            Assert.That(Students_Names, Is.Unique);
 
         }
+       
     }
+    
 }
+
 
--- Teachers_Reports/Main_c.cs
+++ Teachers_Reports/Main_c.cs
@@ -7,213 +7,72 @@ using System.IO;
 
 namespace Teachers_Reports
 {
-
-    public class Main_c
+    public class Main_c { 
+    
+    new Dictionary<string, Marks> Students = new Dictionary<string, Marks>();
+        
+    struct Marks
+      {
+         public int Math;
+         public int Language;
+      }
+    
+    public void ReadMarks(string file_name)
     {
-        static Dictionary<string, Marks> Students = new Dictionary<string, Marks>();
-        struct Marks
-        {
-            public int Math;
-            public int Language;
-        }
-        static void ReadMarks(string file_name)
-        {
+
+
             List<string> lines = File.ReadAllLines(file_name).ToList();
             string subject = lines[0];
             for (int i = 1; i < lines.Count; i++)
-            {
-                string name;
-                string mark;
-                int delim_pos = lines[i].IndexOf('-');
+        {
+            string name;
+            string mark;
+            int delim_pos = lines[i].IndexOf('-');
                 name = lines[i].Substring(0, delim_pos);
                 mark = lines[i].Substring(delim_pos + 1);
 
 
             if (!Students.ContainsKey(name))
-                {
-                    Students.Add(name, new Marks());
-                }
+            {
+                Students.Add(name, new Marks());
+            }
+            
+            Marks temp = Students[name];
+            switch (subject)
+            {
+                case "Math":
 
-                Marks temp = Students[name];
-                switch (subject)
-                {
-                    case "Math":
-
-                        temp.Math = Convert.ToInt32(mark);
-                        Students[name] = temp;
-                        break;
+                    temp.Math = Convert.ToInt32(mark);
+                    Students[name] = temp;
+                    break;
 
 
-                    case "Language":
-                        temp.Language = Convert.ToInt32(mark);
-                        Students[name] = temp;
-                        break;
-
-                }
+                case "Language":
+                    temp.Language = Convert.ToInt32(mark);
+                    Students[name] = temp;
+                    break;
             }
         }
-        public static void Main(string[] args)
-        {
-            File.Delete("final.txt");
-            ReadMarks("subject1.txt");
-            ReadMarks("subject2.txt");
-            
+    }
 
+    public void CountAvarageMark(string final_file_name)
+        {
+            File.AppendAllText(final_file_name, "Avarage\r\n");
             foreach (KeyValuePair<string, Marks> pair in Students)
             {
                 double AvarageMark = (pair.Value.Math + pair.Value.Language) / 2;
-                File.AppendAllText("final.txt", $"Student {pair.Key} has Avarage Mark {AvarageMark.ToString()} \r\n");
+                File.AppendAllText(final_file_name, $"{pair.Key}-{AvarageMark.ToString()} \r\n");
             }
-
+        }
+    public static void Main(string[] args)
+        {
+            File.Delete("final.txt");
+            Main_c student = new Main_c();
             
+            student.ReadMarks("subject1.txt");
+            student.ReadMarks("subject2.txt");
 
+            student.CountAvarageMark("final.txt");
         }
     }
 }
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            /*Dictionary<string, int> Students1 = new Dictionary<string, int>()
-        {
-            {"Ivanov", 1},
-            {"Petrov", 2},
-            {"Sidorov", 3}
-        };
-            Dictionary<string, int> Students2 = new Dictionary<string, int>()
-        {
-            {"Ivanov", 1},
-            {"Dudkov", 2},
-            {"Sidorov", 3}
-        };
-
-            Dictionary<string, int> Students3 = new Dictionary<string, int>()
-        {
-            {"Ivanov",1},
-            {"Dudkov",2},
-            {"Koltsov",3}
-        };
-
-            //Dictionary<int[], string[]> Students4 = new Dictionary<int[], string[]>();
-
-
-
-            bool equals = false;
-
-            if (Students1.Count == Students2.Count || Students1.Count == Students3.Count || Students2.Count == Students3.Count)
-            {
-                equals = true;
-                foreach (var pair in Students1)
-                {
-                    if (!Students2.ContainsKey("Ivanov"))
-                    {
-                        Students1.Add("Ivanov", 1);
-                    }
-                    if (!Students2.ContainsKey("Petrov"))
-                    {
-                        Students1.Add("Petrov", 1);
-                    }
-                    if (!Students2.ContainsKey("Sidorov"))
-                    {
-                        Students1.Add("Sidorov", 1);
-                    }
-                    if (!Students3.ContainsKey("Dudkov"))
-                    {
-                        Students1.Add("Dudkov", 1);
-                    }
-                    if (!Students2.ContainsKey("Koltsov"))
-                    {
-                        Students1.Add("Koltsov", 1);
-                    }
-                }
-            }
-
-            File.WriteAllLines("all.txt", Students1 => "[" + Students1.Key + " " + Students1.Value + "]".ToArray());
-        
-        
-    }
-}
-
-
-
-
-
-/*switch
-{
-    case 1{
-        if (Students1.Values == Students2.Values)
-        Students4.Add(Students1.Keys, Students1.Values);
-            break;
-        }
-
-
-
-}
-
-
-}
-}
-}
-}*/
-
-
-
-
-
-
-
-/*void ReadMarks(string[] file_name)
-        {
-            var dictionary = new ().Deserialize<Dictionary<int, string>>(File.ReadAllText("subject1.txt"));
-            file_name = File.ReadAllLines("subject1.txt");
-            List<string> lines = new List<string>();
-
-            foreach (string s in lines)
-            {
-                Students.Add(1, s);
-            }
-
-
-
-            //string subject = lines[0];
-
-
-
-
-            /*for (int i =1; .....)
-                {
-                    subject.Substring()
-                }
-
-
-                switch
-                {
-
-                }
-
-                  subject = file_name[0];
-
-
-        */
-
